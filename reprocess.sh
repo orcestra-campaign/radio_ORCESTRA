97f4c0d1be267c8244b19bdaf54d1d7b63bd140e
Marius Rixen <marius.rixen@mpimet.mpg.de>
2024-09-12 10:51:55 -0400

    reprocess test

--- reprocess.sh
+++ reprocess.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-#module load nco
+#/bin/bash
 #pip install pysonde
-sounding_converter -i ../../Pysonde_cor/Data/level0/*.cor -o "../Data_radio_ORCESTRA/level1/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc" -c config/main_cor.yaml
-sounding_converter -i ../Data_radio_ORCESTRA/level1/*.nc -o "../Data_radio_ORCESTRA/level2/RS_{campaign}_{platform}_L2_%Y%m%dT%H%M_{direction}.nc" -c config/main_cor.yaml
-ncrcat -h ../Data_radio_ORCESTRA/level2/RS_*L2_*.nc ../Data_Radio_ORCESTRA/concatenated/RS_ORCESTRA_soundings_level2_v1.0.0.nc
+sounding_converter -i ./data/level0/*.mwx -o "./data/level1/RS_{campaign}_{platform}_L1-{direction}_%Y%m%dT%H%M_{version}.nc" -c config/main.yaml
+sounding_converter -i ./data/level0/*.cor -o "./data/level1/RS_{campaign}_{platform}_L1-{direction}_%Y%m%dT%H%M_{version}.nc" -c config/main.yaml
+sounding_converter -i ./data/level1/*.nc -o "./data/level2/RS_{campaign}_{platform}_L2-{direction}_%Y%m%dT%H%M_{version}.nc" -c config/main.yaml
+ncrcat -h ./data/level2/ORCESTRA__*L2-*.nc ./data/level2/RS_{campaign}_level2_v1.0.0.nc
 #rm ./data/level2/*_????????T????_*.nc
 #zip -r data.zip data
 
