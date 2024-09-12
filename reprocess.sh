4ff0cfd0fae8ad26d01629f8ff6c048c45a0afc3
Marius Rixen <marius.rixen@mpimet.mpg.de>
2024-09-12 13:57:29 -0400

    add platform_name as coordinate

--- reprocess.sh
+++ reprocess.sh
@@ -1,9 +1,16 @@
 #/bin/bash
 #pip install pysonde
-sounding_converter -i ./data/level0/*.mwx -o "./data/level1/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc" -c config/main.yaml
-sounding_converter -i ./data/level0/*.cor -o "./data/level1/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc" -c config/main.yaml
-sounding_converter -i ./data/level1/*.nc -o "./data/level2/RS_{campaign}_{platform}_L2_%Y%m%dT%H%M_{direction}.nc" -c config/main.yaml
-ncrcat -h ./data/level2/ORCESTRA__*L2-*.nc ./data/level2/RS_{campaign}_level2_v1.0.0.nc
+#sounding_converter -i ../data/level0/*.mwx -o "../data/level1_mwx/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc" -c config/main_mwx.yaml
+#sounding_converter -i ../data/level0/*.cor -o "../data/level1_cor/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc" -c config/main_cor.yaml
+#sounding_converter -i ../data/level1_mwx/*.nc -o "../data/level2/RS_{campaign}_{platform}_L2_%Y%m%dT%H%M_{direction}.nc" -c config/main_mwx.yaml
+#sounding_converter -i ../data/level1_cor/*.nc -o "../data/level2/RS_{campaign}_{platform}_L2_%Y%m%dT%H%M_{direction}.nc" -c config/main_cor.yaml
+
+# Directory containing the level2 files
+directory_level2="../data/level2"
+# Execute the script to add the launhcing platform as a coordinate
+python ./platform_coordinate/add_platform_coordinate.py "$directory_level2"
+
+ncrcat -h ../data/level2/RS_*L2_*.nc ../data/concatenated/RS_ORCESTRA_level2_v1.0.0.nc
 #rm ./data/level2/*_????????T????_*.nc
 #zip -r data.zip data
 
