f5449fc0c935fa1dd0dc7f21cb5d8d3ea1d29865
Marius Rixen <marius.rixen@mpimet.mpg.de>
2024-09-12 14:00:22 -0400

    uncomment reprocess

--- reprocess.sh
+++ reprocess.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 #pip install pysonde
-#sounding_converter -i ../data/level0/*.mwx -o "../data/level1_mwx/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc" -c config/main_mwx.yaml
-#sounding_converter -i ../data/level0/*.cor -o "../data/level1_cor/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc" -c config/main_cor.yaml
-#sounding_converter -i ../data/level1_mwx/*.nc -o "../data/level2/RS_{campaign}_{platform}_L2_%Y%m%dT%H%M_{direction}.nc" -c config/main_mwx.yaml
-#sounding_converter -i ../data/level1_cor/*.nc -o "../data/level2/RS_{campaign}_{platform}_L2_%Y%m%dT%H%M_{direction}.nc" -c config/main_cor.yaml
+sounding_converter -i ../data/level0/*.mwx -o "../data/level1_mwx/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc" -c config/main_mwx.yaml
+sounding_converter -i ../data/level0/*.cor -o "../data/level1_cor/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc" -c config/main_cor.yaml
+sounding_converter -i ../data/level1_mwx/*.nc -o "../data/level2/RS_{campaign}_{platform}_L2_%Y%m%dT%H%M_{direction}.nc" -c config/main_mwx.yaml
+sounding_converter -i ../data/level1_cor/*.nc -o "../data/level2/RS_{campaign}_{platform}_L2_%Y%m%dT%H%M_{direction}.nc" -c config/main_cor.yaml
 
 # Directory containing the level2 files
 directory_level2="../data/level2"
